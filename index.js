c2abff66cb6b614c2b2155ffb16e9dd04231aed4
Prajeeth Kumar <prajeeth@nunc.local>
2020-12-16 16:38:26 +0530

    json file updated,styles,swapping video

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let body = document.querySelector("body");
-let navbar = document.querySelector(".navbar");
-let navAtags = document.querySelectorAll(".navbar a");
-let footer = document.querySelector("footer");
-let submit = document.querySelector("input[type = submit]");
-let themesContainer = document.querySelector(".theme-container");
-let posterContainer = document.querySelector(".Poster-container");
-let form = document.querySelector("form");
+let navbar = body.querySelector(".navbar");
+let navAtags = body.querySelectorAll(".navbar a");
+let footer = body.querySelector("footer");
+let submit = body.querySelector("input[type = submit]");
+let themeItems = body.querySelectorAll(".theme");
+let themesContainer = body.querySelector(".theme-container");
+let posterContainer = body.querySelector(".Poster-container");
+let form = body.querySelector("form");
 let validators = ["", null, undefined];
+let mainVideo = body.querySelector(".main-video");
+let mainVTitle = body.querySelector(".main-vid-title");
+let mainVDes = body.querySelector(".main-vid-des");
+let left = body.querySelector(".carousel-ctrls .left");
+let right = body.querySelector(".carousel-ctrls .right");
 
 let currentTheme = localStorage.getItem("theme");
 if (currentTheme) changeThemeColor(currentTheme);
@@ -20,6 +26,9 @@ function changeThemeColor(color) {
   submit.style.cssText = defaultStyle;
   navAtags.forEach((a) => {
     a.style.cssText = "color : white";
+  });
+  themeItems.forEach((item) => {
+    item.style.color = color;
   });
 }
 
@@ -41,40 +50,88 @@ fetch("themes.json")
     });
   });
 
+function setAttributes(tag, attrbs) {
+  for (let prop in attrbs) {
+    tag.setAttribute(prop, attrbs[prop]);
+  }
+}
+
+function renderMainVideo(element) {
+  let video = mainVideo.querySelector("video");
+  console.log(video);
+  if (video) {
+  } else {
+    video = document.createElement("video");
+  }
+  let attr = {
+    src: element.Video,
+    poster: element.Poster,
+    width: "100%",
+    height: "auto",
+  };
+  setAttributes(video, attr);
+  let iTag = document.createElement("i");
+  iTag.setAttribute(
+    "class",
+    "fa fa-play-circle fa-4x play-btn pointer text-light"
+  );
+  mainVideo.appendChild(video);
+  mainVideo.appendChild(iTag);
+  mainVTitle.textContent = element.Title;
+  mainVDes.textContent = element.Description;
+}
+
+function renderCarouselImages(element, count) {
+  let divFlexItem = document.createElement("div");
+  if (parseInt(element.id) !== count) {
+    divFlexItem.setAttribute("class", "mr-3");
+  }
+  let divPoster = document.createElement("div");
+  divPoster.setAttribute("class", "poster");
+  let img = document.createElement("img");
+  let attr = {
+    src: element.Poster,
+    class: "pointer",
+    alt: element.Title,
+    onclick: "videoRender(" + element.id + ")",
+  };
+
+  setAttributes(img, attr);
+  let iTag = document.createElement("i");
+  iTag.setAttribute(
+    "class",
+    "fa fa-play-circle fa-2x play-btn pointer text-info"
+  );
+  let divTitle = document.createElement("div");
+  divTitle.setAttribute("class", "vid-title my-3 theme");
+  divTitle.textContent = element.Title;
+  let divDes = document.createElement("div");
+  divDes.setAttribute("class", "vid-des mb-3 text-secondary");
+  divDes.textContent = element.Description;
+  divPoster.innerHTML += img.outerHTML + iTag.outerHTML;
+  divFlexItem.innerHTML +=
+    divPoster.outerHTML + divTitle.outerHTML + divDes.outerHTML;
+  posterContainer.appendChild(divFlexItem);
+}
+
 // Video
-fetch("videoInfo.json")
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((element) => {
-      let divFlexItem = document.createElement("div");
-      if (parseInt(element.id) !== data.length) {
-        divFlexItem.setAttribute("class", "mr-3");
-      }
-      let divPoster = document.createElement("div");
-      divPoster.setAttribute("class", "poster");
-      let img = document.createElement("img");
-      img.setAttribute("src", element.Poster);
-      img.setAttribute("class", "pointer");
-      img.setAttribute("alt", element.Title);
-      let iTag = document.createElement("i");
-      iTag.setAttribute(
-        "class",
-        "fa fa-play-circle fa-2x play-btn pointer text-light"
-      );
-      let divTitle = document.createElement("div");
-      divTitle.setAttribute("class", "main-vid-title my-3");
-      divTitle.textContent = element.Title;
-      let divDes = document.createElement("div");
-      divDes.setAttribute("class", "main-vid-des mb-3 text-secondary");
-      divDes.textContent = element.Description;
-      divPoster.appendChild(img);
-      divPoster.appendChild(iTag);
-      divFlexItem.appendChild(divPoster);
-      divFlexItem.appendChild(divTitle);
-      divFlexItem.appendChild(divDes);
-      posterContainer.appendChild(divFlexItem);
+function videoRender(id = null) {
+  fetch("videoInfo.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let count = data.lenth;
+      id = id === null ? 1 : id;
+      data.forEach((element) => {
+        if (element.id === id) {
+          renderMainVideo(element);
+        } else {
+          renderCarouselImages(element, count);
+        }
+      });
     });
-  });
+}
+
+videoRender();
 
 const exceptionMsg = {
   firstname: "Please Enter First Name",
@@ -87,7 +144,7 @@ form.addEventListener("submit", validate);
 
 function validate(e) {
   e.preventDefault();
-  let expMsgs = document.querySelectorAll("small");
+  let expMsgs = document.querySelectorAll(".form-group small");
   expMsgs.forEach((element) => {
     element.textContent = "";
   });
@@ -95,11 +152,11 @@ function validate(e) {
   let small = document.createElement("small");
   small.setAttribute("class", "text-danger");
 
-  let firstnameInp = form.querySelector("input#firstname");
-  let lastnameInp = form.querySelector("input#lastname");
-  let emailInp = form.querySelector("input#email");
-  let phoneInp = form.querySelector("input#phone");
-  let commentsInp = form.querySelector("textarea#comments");
+  let firstnameInp = form.querySelector("#firstname");
+  let lastnameInp = form.querySelector("#lastname");
+  let emailInp = form.querySelector("#email");
+  let phoneInp = form.querySelector("#phone");
+  let commentsInp = form.querySelector("#comments");
 
   let fstname = firstnameInp.value;
   let lstname = lastnameInp.value;
@@ -126,4 +183,16 @@ function validate(e) {
   } else {
     console.log("Submit form");
   }
+}
+
+left.addEventListener("click", scrollRight);
+right.addEventListener("click", scrollLeft);
+
+function scrollRight() {
+  posterContainer.scrollLeft -= 600;
+  console.log("clicked left");
+}
+function scrollLeft() {
+  posterContainer.scrollLeft += 600;
+  console.log("clicked right");
 }
